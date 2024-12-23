b4c06a8fcd188d8b3158c0d89d4f730d9f0c8325
cruzayye <johnnyc010392@gmail.com>
2024-12-23 06:51:45 -0800

    new video with lower resolution

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,21 +1,27 @@
 import './Home.scss'
-import BackgroundVideo from '../Assets/backgroundClip.mp4'
+import BackgroundVideo from '../Assets/backgroundClip_lowBitrate.mp4'
 
 const Home = () => {
-    return (
-        <div className="background-container">
-          {/* Video Background */}
-          <video autoPlay loop muted playsInline className="background-video">
-            <source src={BackgroundVideo} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          
-          {/* Content */}
-          <div className="content">
-            <h1>LION MANE</h1>
-          </div>
-        </div>
-      );
- }
+  return (
+    <div className="background-container">
+      {/* Video Background */}
+      <video autoPlay loop muted playsInline className="background-video">
+        <source src={BackgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
- export default Home;
+      {/* Content */}
+      <div className="content">
+        <h1>LION MANE</h1>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <h2>book now</h2>
+        </a>
+      </div>
+
+    </div>
+
+
+  );
+}
+
+export default Home;
