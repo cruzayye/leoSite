86dd4382e42bc4aac085e6ce6d68fd4679a581c5
cruzayye <johnnyc010392@gmail.com>
2025-02-12 18:03:39 -0800

    more changes to build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "src",
   build: {
-    outDir: 'dist' 
+    outDir: '../dist'  // This ensures the dist folder is created in the project root
   }
 });
