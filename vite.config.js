f5c84dbee22115efb417a6387684d021b3fc4773
cruzayye <johnnyc010392@gmail.com>
2025-03-11 10:07:53 -0700

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
   root: "src",
   build: {
     outDir: '../dist'  // This ensures the dist folder is created in the project root
-  }
+  },
+  assetsInclude: ['**/*.PNG']
 });
