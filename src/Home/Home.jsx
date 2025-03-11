1f881a9cfc2aedbd1773efaaa07cbbf7a15c1d74
cruzayye <johnnyc010392@gmail.com>
2025-03-11 09:50:42 -0700

    fixed sources

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="background-container">
       {/* Video Background */}
       <video autoPlay loop muted playsInline className="background-video">
-        <source src={'../Assets/backgroundClip_lowBitrate2.mp4'} type="video/mp4" />
+        <source src={'../../Assets/backgroundClip_lowBitrate2.mp4'} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
