73cb98bb5853752ca2cdbfdecdcdde92f511170e
cruzayye <johnnyc010392@gmail.com>
2025-03-22 14:58:44 -0700

    redirects config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Create _redirects file for Netlify
+const writeRedirectsFile = () => {
+  const redirectsContent = "/* /index.html 200";
+  const outDir = "../dist";
+
+  return {
+    name: "write-redirects-file",
+    closeBundle() {
+      fs.writeFileSync(
+        resolve(__dirname, outDir, "_redirects"),
+        redirectsContent
+      );
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), writeRedirectsFile()],
   root: "src",
   build: {
-    outDir: '../dist'  // This ensures the dist folder is created in the project root
+    outDir: "../dist", // This ensures the dist folder is created in the project root
   },
-  assetsInclude: ['**/*.PNG']
+  assetsInclude: ["**/*.PNG"],
 });
