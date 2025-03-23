23a0a7f7ae00acf7ddf1eb3ed07da81713fec450
cruzayye <johnnyc010392@gmail.com>
2025-03-23 16:28:12 -0700

    added toml file

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path';
-import fs from 'fs';
 
 
-// Create _redirects file for Netlify
-const writeRedirectsFile = () => {
-  const redirectsContent = "/* /index.html 200";
-  const outDir = "../dist";
-
-  return {
-    name: "write-redirects-file",
-    closeBundle() {
-      fs.writeFileSync(
-        resolve(__dirname, outDir, "_redirects"),
-        redirectsContent
-      );
-    },
-  };
-};
 
 export default defineConfig({
-  plugins: [react(), writeRedirectsFile()],
+  plugins: [react()],
   root: "src",
   build: {
-    outDir: "../dist", // This ensures the dist folder is created in the project root
+    outDir: '../dist'  // This ensures the dist folder is created in the project root
   },
-  assetsInclude: ["**/*.PNG"],
+  assetsInclude: ['**/*.PNG']
 });
